a3b070dc21a648d2a3597a2c6ea368e0dcded43f
zulaikha <ikanaza5090@gmail.com>
2018-01-03 11:10:42 +0800

    done

--- student/urls.py
+++ student/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.home,name='student_home'),
 	url(r'^home/$',views.home_json,name='student_home_json'),
+	url(r'^new/$', views.student_new, name='student_new'),
 	url(r'^list_json/$',views.student_list_json.as_view(), name="student_list_json"),
-	]
+	url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
+]
--- student/views.py
+++ student/views.py
@@ -3,26 +3,41 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 #from django.utils import timezone
 from .models import Student
-#from .forms import MessageForm, SearchForm, StudentForm
+from .forms import StudentForm
 #from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.db.models import Count, Sum, Q, Case, Value, When, IntegerField
 
-from .models import Student
+
 # Create your views here.
-
-
 def home(request):
 	return render(request,'base.html')
 
 def home_json(request):
 	return render(request,'student/home_json.html')
 
+def student_new(request):
 
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student = form.save(commit=False)
+            student.createdby = request.user
+            student.save()
+             # return redirect('post_detail', pk=post.pk)
+            messages.success(request, "Student record with ID: " + str(student.pk) + " has been created ! ")
+            #return redirect(reverse_lazy('student_detail',kwargs={'pk': student.pk }))
+    else:
+        form = StudentForm()
+    print(request.user)
+    return render(request, 'student/student_new.html', {'form': form})
 
+def student_detail(request,pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'student/student_detail.html', {'student': student})
 
-# Student JSON list filtering
+# Student JSON list filtering      
 class student_list_json(BaseDatatableView):
     order_columns = ['icnum','name','course', 'pk','link']
 
@@ -96,8 +111,8 @@ class student_list_json(BaseDatatableView):
                 # item.course,
                 item.get_course_display(),
                 str(item.pk),
-                #reverse_lazy('student_detail',kwargs={'pk': str(item.pk)})
-                reverse_lazy('student_home')
+                reverse_lazy('student_detail',kwargs={'pk': str(item.pk)})
+                #reverse_lazy('student_home')
             ])
             # print(json_data)
         return json_data
